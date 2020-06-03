6c1a3458a2a211aa06e2d5a9c825825c96b5e2d0
haccount <hlewisacc@gmail.com>
2020-06-02 19:46:53 -0700

    add handle GET request to index.js

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 // set up express app
 const app = express();
 
+// handle GET request
+app.get('/', function(req, res){
+  console.log('GET request');
+  res.send({name: 'John'});
+});
+
 // listen for requests
 app.listen(process.env.port || 3001, function(){
   console.log("Now listening for requests");
