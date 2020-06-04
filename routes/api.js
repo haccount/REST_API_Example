e693f9f6d03b9dc1f21ca8f38a6838c8b045c6c6
haccount <hlewisacc@gmail.com>
2020-06-04 02:30:14 -0700

    update POST request so it can save data to db

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Sample = require('../models/samples')
 
 // instead of app.get, we attach the methods to the route objecs set above,  the following:
 // get a list of stuff from the db
@@ -9,12 +10,23 @@ router.get('/sample', function(req, res){
 
 // add new stuff to the db
 router.post('/sample', function(req, res){
-  console.log(req.body);
-  res.send({
-    type: 'POST',
-    name: req.body.name,
-    rank: req.body.rank
-  });
+  // console.log(req.body);
+
+  Sample.create(req.body).then(function(sample){
+    res.send(sample
+    //   {
+    //   type: 'POST',
+    //   name: req.body.name,
+    //   rank: req.body.rank
+    // }
+    );
+  }); 
+  //creates a new instance of the Sample object locally and saves to the database
+  // a promise is returned, we need to wait for the action to be completed, so tack on .then method, function only fires once the action has been completed, then send back the sample that we've save to the db.
+
+  // var sample = new Sample(req.body);
+  // sample.save();
+  // can save with the code above, but even easier way to do it with mongoose method, create (above)
 });
 
 // update stuff in the db
