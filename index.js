42f82b086dcc68599cb98be717f8b752752cb828
haccount <hlewisacc@gmail.com>
2020-06-04 02:31:41 -0700

    modify index.js to connect to mongodb

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 // const routes = require('./routes/api');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // set up express app
 const app = express();
+
+// connect to mongodb
+// setting up db as 'samplego'; doesn't matter that it doesn't exist yet, mongoose will go out and create it
+mongoose.connect('mongodb://localhost/samplego')
+// overwrite mongoose promise because it is deprecated
+mongoose.Promise = global.Promise;
+
 
 app.use(bodyParser.json());
 
